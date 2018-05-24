23be45431146416140f81517a7918f24defc3c99
CRiley <CRRiley12790@gmail.com>
2018-05-24 19:18:20 -0400

    Homework5

---
+++ javascript/app.js
@@ -0,0 +1,41 @@
+var triviaQuestions = [{
+	question: "What NBA playoff team blew a 3-1 lead in the 2016 NBA Finals?",
+	answerList: ["Cleveland Cavaliers", "Golden State Warriors", "San Antonio Spurs", "Miami Heat"],
+	answer: 2
+},{
+	question: "Who is the Quarterback of the Green Bay Packers?",
+	answerList: ["Aaron Rodgers", "Eli Manning", "Brett Favre", "Andrew Luck"],
+	answer: 0
+},{
+	question: "Who won the MLB American League Cy Young in 2016?",
+	answerList: ["Corey Kluber", "Clayton Kershaw", "Justin Verlander", "Carlos Carrasco"],
+	answer: 0
+},{
+	question: "Which current NFL Quarterback has the most Super Bowl rings?",
+	answerList: ["Cam Newton", "Eli Manning", "Tom Brady", "Aaron Rodgers"],
+	answer: 2
+},{
+	question: "Which Ohio city did LeBron James grow up?",
+	answerList: ["Cleveland, OH", "Cincinnati, OH", "Columbus, OH", "Akron, OH"],
+	answer: 3
+},{
+	question: "Who hit the game winning shot in game 7 of the 2016 NBA Finals?",
+	answerList: ["Kyrie Irving", "LeBron James", "Dwayne Wade", "Kevin Love"],
+	answer: 0
+},{
+	question: "Which MLB player is known as 'The Machine'?",
+	answerList: ["Aaron Judge", "Albert Pujols", "Alex Rodriguez", "Nolan Arenado"],
+	answer: 1
+},{
+	question: "Who is the head football coach of the Ohio State Buckeye football team?",
+	answerList: ["Hue Jackson", "Nick Saban", "Urban Meyer", "Pete Carroll"],
+	answer: 2
+},{
+	question: "What is the name of the Alabama vs. Auburn college football rivalry?",
+	answerList: ["Super Bowl", "Iron Bowl", "Rose Bowl", "Suger Bowl"],
+	answer: 1
+},{
+	question: "What city is home of the Golden State Warriors?",
+	answerList: ["Seattle, WA", "Houston, Tx", "Los Angeles, CA", "San Francisco, CA"],
+	answer: 3
+}]
